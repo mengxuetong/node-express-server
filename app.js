4646c3198669a686da839c09e1e7d0987cdcc3ec
mengxuetong <mengxuetong@foxmail.com>
2015-10-29 14:41:18 +0800

    update

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 var http = require('http'),
     wechat = require('node-wechat');
-var PORT = 3030;
+
 wechat.token = 'mengxt';
+
 http.createServer(function (req, res) {
   //检验 token
   wechat.checkSignature(req, res);
   //预处理
   wechat.handler(req, res);
 
-  //链式监听
+  //监听文本信息
   wechat.text(function (data) {
-    // TODO
-  }).image(function (data) {
-    // TODO
-  }).location(function (data) {
-    // TODO
-  }).link(function (data) {
-    // TODO
-  }).event(function (data) {
-    // TODO
-  }).voice(function (data) {
-    // TODO
-  }).video(function (data) {
-    // TODO
-  }).all(function (data) {
+    //console.log(data.ToUserName);
+    //console.log(data.FromUserName);
+    //console.log(data.CreateTime);
+    //console.log(data.MsgType);
+    //...
     var msg = {
       FromUserName : data.ToUserName,
       ToUserName : data.FromUserName,
-      //MsgType : "news",
-      Articles : ""
+      //MsgType : "text",
+      Content : "这是文本回复",
+      //FuncFlag : 0
     }
+    //回复信息
     wechat.send(msg);
   });
 
-}).listen(PORT,function(){
-    console.log('http server is started on port'+PORT)
-});
+  //监听图片信息
+  //wechat.image(function (data) { ... });
+
+  //监听地址信息
+  //wechat.location(function (data) { ... });
+
+  //监听链接信息
+  //wechat.link(function (data) { ... });
+
+  //监听事件信息
+  //wechat.event(function (data) { ... });
+
+  //监听语音信息
+  //wechat.voice(function (data) { ... });
+
+  //监听视频信息
+  //wechat.video(function (data) { ... });
+
+  //监听所有信息
+  //wechat.all(function (data) { ... });
+}).listen(80);
